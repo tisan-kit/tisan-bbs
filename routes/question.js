628583c12f436d824db735a0e12fb6efbb25acab
bugall <bugall@sina.com>
2015-11-30 00:34:42 +0800

    add backbone.js

--- routes/question.js
+++ routes/question.js
@@ -2,27 +2,44 @@ var express = require('express');
 var router = express.Router();
 var Question = require('../models/mysql/question').Question;
 var QuestionContent = require('../models/mysql/question_content').QuestionContent;
+//render页面
 
-router.get('/',function(req,res){
-  res.render('question');
+router.get('/test',function(req,res){
+  res.render('test');
 })
-router.get('/question/info', function(req, res) {
+router.get('/question/list', function(req, res) {
   Question.findAll({}).then(function(questionInfo){
     var tmpQuestionInfo=[];
     for(var i in questionInfo){
-      if(!!questionInfo[i].dataValues.length){
         tmpQuestionInfo.push(questionInfo[i].dataValues);
-      }
     }
     res.json({
       code:0,
       msg:'ok',
-      questionInfo:tmpQuestionInfo
+      data:{
+        questionInfo:tmpQuestionInfo
+      }
     });
   }).catch(function(err){
     console.log(err);
   })
 });
+
+router.get('/question/info/:id',function(req,res){
+  var id = req.params['id'];
+  if(!isNaN(id)){
+    Question.find({where:{id:id}}).then(function(result){
+      res.json({ msg:'ok', code:0, data:{
+          questionInfo:result.dataValues
+        }
+      })
+    }).catch(function(err){
+      console.log(err);
+    })
+  }else{
+    res.json({ msg:'data format error', data:'', code:1, })
+  }
+})
 
 //问题主页
 router.get('/publish', function(req, res) {
@@ -47,7 +64,13 @@ router.post('/publish', function(req, res) {
     console.log(err);
   })
 });
+
+router.get('/',function(req,res){
+  res.render('question');
+})
 router.get('/question/:id',function(req,res){
-  res.render('question_content');
+  res.render('question_content',{
+    question_id:req.params['id']
+  });
 })
 module.exports = router;
